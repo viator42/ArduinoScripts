23c3a737e2565ef0445a13d385d0fb9909a94292
Chuanqing.Wu <via313@gmail.com>
2014-07-10 02:08:57 +0800

    双电机控制完成

--- MotorL298N/MotorL298N.ino
+++ MotorL298N/MotorL298N.ino
@@ -1,18 +1,39 @@
-int EN1 = 11;
+
+int speed1 = 128;
+int speed2 = 255;
+
+boolean direction1 = true;
+boolean direction2 = true;
+
 void setup() {
   // set the digital pin as output:
-  pinMode(13, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  
   pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+  
 }
- 
+
+void motorSet(int en, int in1, int in2, boolean dir, int speed)
+{
+  if(dir == true)
+  {
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, HIGH);
+  }
+  else
+  {
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
+  }
+  
+  analogWrite(en, speed);
+}
+
 void loop()
 {
-  int value;
-   for(value = 0 ; value <= 255; value+=5)
-    {
-    digitalWrite(13, HIGH);
-    digitalWrite(12, LOW);
-    analogWrite(EN1, value);
-    delay(250);
-    }
+  motorSet(8, 9, 10, true, 255);
+  motorSet(11, 12, 13, true, 255);
+  
 }
